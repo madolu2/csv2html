4727a03613066922d28e7e6394d3dfe6578e1300
madolu2 <unclemadolu2@gmail.com>
2020-04-18 16:37:50 +0700

    Add - Created html_wrapper.py -> Created 5 funcs for wrapping data & generating HTML document

---
+++ html_wrapper.py
@@ -0,0 +1,58 @@
+class HTMLWrapper():
+    def wrap_tags(self, tag_array, tag_name):
+        wrapped_tag_array = []
+        for tag in tag_array:
+            wrapped_tag_array.append(f"\t\t<{tag_name}>{tag}</{tag_name}>\n")
+        return wrapped_tag_array
+
+    def wrap_tr(self, tag_array, tag_name):
+        tr = '\t<tr>'
+        tags = self.wrap_tags(tag_array, tag_name)
+        for tag in tags:
+            tr += tag
+        tr += '\t</tr>'
+        return tr
+
+    def wrap_h1(self, h1):
+        return f'<h1  class="sign">{h1}</h1>'
+
+    def generate_table(self, th_array, td_arrays):
+        table = "<table>\n"
+
+        table += self.wrap_tr(th_array, 'th')
+        for td_array in td_arrays:
+            table += self.wrap_tr(td_array, 'td')
+
+        table += "</table>"
+        return table
+
+    def generate_html_document(self, th_array, td_arrays):
+        document = '''<!doctype html >
+        <html lang = "en" >
+        <head >
+        <title > Title < /title >
+
+        <meta charset = "utf-8" >
+        <meta name = "viewport" content = "width=device-width, initial-scale=1, shrink-to-fit=no" >
+        <link rel = "stylesheet" href = "static/base.css" >
+        <link rel = "stylesheet" href = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity = "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin = "anonymous" >
+        </head >'''
+
+        document += self.generate_table(th_array, td_arrays)
+
+        document += '''
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+        </script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+        </script>
+        </body>
+
+        </html>
+        '''
+        return document
