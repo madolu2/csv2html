20759ba0e3d8068721e692b70f381c5c1b59a175
madolu2 <unclemadolu2@gmail.com>
2020-04-18 17:23:12 +0700

    Fix - Add sript src line in generate_html_document.document

--- html_wrapper.py
+++ html_wrapper.py
@@ -42,7 +42,8 @@ class HTMLWrapper():
 
         document += self.generate_table(th_array, td_arrays)
 
-        document += '<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"' \
+        document += '<script src="../static/base.js"></script>' \
+        '<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"' \
         'integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">' \
         '</script>' \
         '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"' \
