7dc8f17dc9782a0d09c65104e67e8b879b0bd754
madolu2 <unclemadolu2@gmail.com>
2020-04-22 20:39:27 +0700

    Refactor - Changed html structure. Added bs4 classes. Refactored base file. Deleted create_input file

--- web/static/base.js
+++ web/static/base.js
@@ -1,11 +1,12 @@
 let btn_offer = document.querySelector(".offer"),
+    btn_accept = document.querySelector('.accept'),
     form_offer = document.querySelector(".form"),
     btn_decline = document.querySelector(".decline"),
     new_offer = document.querySelectorAll("input"),
     table = document.querySelector("tbody"),
     tr_values = document.querySelectorAll("tr");
 
-document.querySelectorAll('tr').forEach(element => {
+tr_values.forEach(element => {
     if (element.id != "main-cells") {
         element.addEventListener('mouseover', () => {
             children = Array.from(element.children)
@@ -29,33 +30,46 @@ document.querySelectorAll('tr').forEach(element => {
 });
 
 btn_offer.addEventListener("click", () => {
-    form_offer.style.display = "flex";
+    if (form_offer.style.display != "grid") {
+        form_offer.style.display = "grid";
+    } else {
+        form_offer.style.display = "none";
+    }
 })
 
 btn_accept.addEventListener("click", () => {
-   new_row = table.appendChild(document.createElement("tr"));
-   new_row.innerHTML = fill_row_values();
-   get_values(tr_values);
+    new_offer.forEach(element => {
+        if (element.value) {
+            new_row = table.appendChild(document.createElement("tr"));
+            new_row.innerHTML = fill_row_values();
+            get_values(tr_values);
+            new_offer.forEach(el => {
+                el.value = '';
+            });
+        }
+    });
 })
 
 btn_decline.addEventListener("click", () => {
-    form_offer.style.display = "none";
+    new_offer.forEach(element => {
+        element.value = '';
+    });
 })
 
-function fill_row_values(){
+function fill_row_values() {
     let string = "";
-    for (var i = 0; i<new_offer.length; i++){
-        string+="<td>"+ new_offer[i].value +"</td>";
+    for (var i = 0; i < new_offer.length; i++) {
+        string += "<td>" + new_offer[i].value + "</td>";
     }
     return string;
 }
 
-function get_values(array){
+function get_values(array) {
     rows = []
     blocks = []
     th = []
-    array.forEach((row)=>{
-        for (var i = 0; i < row.cells.length; i++){
+    array.forEach((row) => {
+        for (var i = 0; i < row.cells.length; i++) {
             blocks.push(row.cells[i].innerHTML);
         }
         rows.push(blocks);
@@ -64,7 +78,7 @@ function get_values(array){
     th = [rows[0]];
     rows.shift()
     table_values = {
-        headers_value:th,
-        tr_values:rows
+        headers_value: th,
+        tr_values: rows
     };
 }
