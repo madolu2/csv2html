02bf0cc62a5a89764cef30e5aec56a4fb91c4d53
madolu2 <unclemadolu2@gmail.com>
2020-05-09 12:59:05 +0700

    Refactor - Rewritten

--- csv2html.py
+++ csv2html.py
@@ -3,31 +3,33 @@ import os
 import html_wrapper
 
 
+
+def create_tag(tag, value):
+    if isinstance(value, list):
+        return f'<{tag}>\n{"".join(value)}\n</{tag}>\n'
+    else:
+        return f'<{tag}>\n{value}\n</{tag}>\n'
+
 html_dir = 'web/templates'
 csv_dir = 'csv'
 
 csv_files = os.listdir(csv_dir)
 
-wrapper = html_wrapper.HTMLWrapper()
-with open(f"{html_dir}/contentPage.html", 'w') as cp:
-    cp.write(wrapper.generate_content_page(csv_files))
-
 for csv_file in csv_files:
     with open(f"{csv_dir}/{csv_file}", 'r') as file:
         csv_file = csv_file.replace('.csv', '')
-        th_array = []
-        td_arrays = []
         csv_dicts = csv.DictReader(file, delimiter=',')
 
-        for csv_dict in csv_dicts:
-            for key in csv_dict:
-                if not key in th_array:
-                    th_array.append(key)
-            td_array = []
-            for key in csv_dict:
-                td_array.append(csv_dict[key])
-            td_arrays.append(td_array)
+        headers = csv_dicts.fieldnames
+        content = [x[y] for x in csv_dicts for y in headers]
 
-        document = wrapper.generate_html_document(th_array, td_arrays, csv_file)
-        with open(f"{html_dir}/{csv_file}.html", 'w', encoding='utf-8') as file:
-            file.write(document)
+        tags = [create_tag('tr', [create_tag('th', header) for header in headers])]
+
+        steps = [i for i in range(0, int(len(content)/len(headers)), len(headers))]
+
+        tags.append(create_tag('tr', [create_tag('td', value) for s in steps for value in content[s:s+len(headers)] ]))
+            
+        table = create_tag('table', [x for x in tags])
+
+    with open('newhtml.html', 'w') as file:
+        file.write(table)
